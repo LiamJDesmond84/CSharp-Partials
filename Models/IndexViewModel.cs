8b61abe9b2daa3262ce981a2b16be25fc6aa3e6f
Liam James Desmond <liamjdesmond84@gmail.com>
2023-06-15 19:53:17 -0500

    Fixed, the platform's info was wrong

--- Models/IndexViewModel.cs
+++ Models/IndexViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class IndexViewModel
     {
-        public Product NewProduct { get; set; }
-        public User NewUser { get; set; }
+        public Product? NewProduct { get; set; }
+        public User? NewUser { get; set; }
     }
 }
