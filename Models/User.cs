f3e64682a90f13ed94abbbea6b430b58aeb6c153
Liam James Desmond <liamjdesmond84@gmail.com>
2023-06-15 19:15:40 -0500

    Updating models

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSharp_Partials.Models
+{
+    public class User
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+}
