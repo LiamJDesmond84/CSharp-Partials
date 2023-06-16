d3b41688ec99fedbcb2a4f3ec53af7929dbdfe0f
Liam James Desmond <liamjdesmond84@gmail.com>
2023-06-15 19:13:54 -0500

    Updating models

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,8 +1,14 @@
-﻿namespace CSharp_Partials.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CSharp_Partials.Models
 {
     public class Product
     {
+        [Required]
+        [Display(Name = "Product Name")]
         public string ProductName { get; set; }
+        [Required]
         public double Price { get; set; }
     }
 }
